a00f51edd862cdc103a2920c95d414d91fb48962
Brandon Huang <byh1@rice.edu>
2023-11-08 21:33:39 -0600

    core functionality

--- pokedex.py
+++ pokedex.py
@@ -1,70 +1,65 @@
-import argparse
 import requests
 import json
-
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('abilities', nargs='+', help='List of abilities to search for')
-    return parser.parse_args()
-
-args = parse_arguments()
+import sys
+from pprint import pprint
 
 
+def find_known_associates(target, allNames):
+    base_name = target.split('-')[0]
+    return [name for name in allNames if ((base_name in name.split('-')) and (target != name))]
 
-def get_pokemon_data(ability):
-    url = f'https://pokeapi.co/api/v2/ability/{ability}/'
-    response = requests.get(url)
 
-    #check code was successful
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        print(f"Failed to fetch data for ability: {ability}")
-        return None
+def find_items(target):
+    url = f'https://pokeapi.co/api/v2/pokemon/{target}'
+    result = requests.get(url).json()
+    pokeItems =  [d.get('item', {}).get('name', None) for d in result['held_items']]
+    height = result.get("height")
+    return pokeItems, height\
 
-#get name of pokemon
-def extract_pokemon_info(ability_data):
-    pokemon_info = []
 
-    for pokemon in ability_data['pokemon']:
-        # Extract the Pokémon name
-        name = pokemon['pokemon']['name']
 
-        # Extract items associated with the Pokémon
-        items = []
-        for item in pokemon['pokemon']['held_items']:
-            items.append(item['item']['name'])
 
-        # Extract known associates (if any)
-        associates = []
-        for associate in pokemon['pokemon']['known_associates']:
-            associates.append(associate['name'])
-
-        # Create a dictionary to store the extracted information
-        pokemon_dict = {
-            'name': name,
-            'items': items,
-            'known_associates': associates
-        }
-
-        # Append the Pokémon dictionary to the list
-        pokemon_info.append(pokemon_dict)
-
-    return pokemon_info
-
-#formate into JSON format
-
-# Fetch data for a specific ability
-ability_data = get_pokemon_data('stench')
-
-# Extract information for the ability
-pokemon_info = extract_pokemon_info(ability_data)
-
-# Format the output for this ability 
-output = {
-    'stench': pokemon_info
-}
-
-# Print or return the output as a JSON object
-print(json.dumps(output, indent=2))  # Use 'json.dumps' to format the output
+def finalResult(abilities):
+    url = f'https://pokeapi.co/api/v2/pokemon/?limit=2000'
+    pokedex = requests.get(url).json()
+    
+    allNames = set([d.get('name') for d in pokedex['results']])
+    finalResult = {}
+    for ability in abilities:
+        url = f'https://pokeapi.co/api/v2/ability/{ability}'
+        result = requests.get(url).json()
+        pokemonWithAbility = [d.get('pokemon', {}).get('name', None) for d in result.get("pokemon")]
+        abilityResult = []
+        heightDict = {}
+        minHeight = 999999999999999999999
+        maxHeight = -9999999999999999999
+        for pokemon in pokemonWithAbility:
+            items, height = find_items(pokemon)
+            heightDict[pokemon] = height
+            pokemonResult = {"name" : pokemon, "known_associates" : find_known_associates(pokemon, allNames), "items" : items}
+            abilityResult.append(pokemonResult)
+        biggestHeights = []
+        smallestHeights = []
+        for pokemon, height in heightDict.items():
+            if height > maxHeight:
+                biggestHeights = [pokemon]
+                maxHeight = height
+            if height == maxHeight:
+                biggestHeights.append(pokemon)
+            if height < minHeight:
+                smallestHeights = [pokemon]
+                minHeight = height
+            if height == minHeight:
+                smallestHeights.append(pokemon)
+        for pokemonResult in abilityResult:
+            if pokemonResult["name"] in biggestHeights:
+                pokemonResult["tallest"] = True
+            if pokemonResult["name"] in smallestHeights:
+                pokemonResult["shortest"] = True
+        finalResult[ability] = abilityResult
+    pprint(finalResult)
+        
+if __name__ == "__main__":
+    #test
+    arg = sys.argv[1:]
+    finalResult(arg)
