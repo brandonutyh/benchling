46ae79393fe04d44e47c617983aac75cefedd492
Brandon Huang <byh1@rice.edu>
2023-11-09 11:26:50 -0600

    docs

--- pokedex.py
+++ pokedex.py
@@ -4,12 +4,30 @@ import json
 import sys
 from pprint import pprint
 
+
 def find_known_associates(target, allNames):
+    """Given target pokemon name, find relaed pokemon, in same "species"
+
+    Args:
+        target (string): pokemon to be examined
+        allNames (_type_): all the existing pokemon names
+
+    Returns:
+        list: all related pokemon to the target
+    """
     base_name = target.split('-')[0]
     return [name for name in allNames if ((base_name in name.split('-')) and (target != name))]
 
 
 def find_items(target):
+    """Find the items that the target pokemon could be holding, also returns and saves the height.
+
+    Args:
+        target (string): pokemon to be examined
+
+    Returns:
+        string: items the target has as well as it's height
+    """
     url = f'https://pokeapi.co/api/v2/pokemon/{target}'
     result = requests.get(url).json()
     pokeItems =  [d.get('item', {}).get('name', None) for d in result['held_items']]
@@ -17,6 +35,14 @@ def find_items(target):
     return pokeItems, height
  
 def get_data(url):
+    """gets data in json format from API request
+
+    Args:
+        url (string): url to API
+
+    Returns:
+        the data from the API call, or exception if there is error
+    """
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -28,6 +54,11 @@ def get_data(url):
 
 
 def final_result(abilities):
+    """Final method outputting the results given the ability input
+
+    Args:
+        abilities (list): list of the abilities to be examined
+    """
     url = f'https://pokeapi.co/api/v2/pokemon/?limit=2000'
     pokedex = get_data(url)
     #pokedex = requests.get(url).json()
